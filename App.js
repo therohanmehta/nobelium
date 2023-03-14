c88b0154229fec1587177cf93be1c6166c91d7bd
therohanmehta <rohandhn786@gmail.com>
2023-03-15 02:42:06 +0530

    This is the solution of the react assignment day 7

--- App.js
+++ App.js
@@ -1,38 +1,79 @@
-import "./App.css";
-import { ValueIncreseDecrease } from "./valueIncreaseDecrease";
-import { NameChanger } from "./nameChanger";
-import { useState } from "react";
+import React, { useState } from 'react';
+import "./App.css"
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [count, setCount] = useState(0)
 
-export default function App() {
-  const [list, setList] = useState([]);
-  const [text, setText] = useState("");
+  const countTask = ()=>{
+    setCount(count+1)
+  }
 
-  function storeItems(event) {
-    setText(event.target.value);
+  const decreaseTask = ()=>{
+    if(count>0){
+        setCount(count-1)
+    }
   }
-  function addToTheList() {
-    setList([...list, text]);
+
+  const decreaseTask2 = (index)=>{
+    const newTasks = [...tasks];
+    newTasks[index].completed ? setCount(count+1):setCount(count-1)
   }
+
+  const handleBoth1 = (index,task)=>{
+    decreaseTask2(index)
+    handleComplete(index)
+  }
+
+  const handleBoth2 = (index)=>{
+    decreaseTask();
+    handleDelete(index)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTasks([...tasks, { text: newTask, completed: false }]);
+    setNewTask('');
+  };
+
+  const handleComplete = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  };
+
+  const handleDelete = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
 
   return (
     <>
-      <h1>ToDo List Maker</h1>
-      <input onChange={storeItems} />
-      <br />
-      <button onClick={addToTheList}>Click to add</button>
-      <ol>
-        {list.map((ele) => (
-          <li>{ele}</li>
+      <h1 style={{paddingTop:10, position: 'sticky', top:0, width:'100%', textAlign: 'center'}}>No. of pending Task ({count})</h1>
+        {tasks.map((task, index) => (
+          <li className='listItem'
+            key={index}
+            style={{ textDecoration: task.completed ? 'line-through' : 'none', listStyleType :'none'}}
+          >
+            <div style={{fontSize:20, overflowWrap: 'break-word', inlineSize: 176}}>{task.text}</div>
+            <div>
+                <button onClick={() => handleBoth1(index,task)}  className='btnDiv'>Complete</button>
+            <button onClick={() => handleBoth2(index)} className='btnDiv'>Delete</button>
+            </div>
+          </li>
         ))}
-      </ol>
-      <hr />
-
-      <h1>Click On the Button to increase or Decrease the Value</h1>
-      <ValueIncreseDecrease />
-
-      <hr />
-
-      <NameChanger />
+     <form onSubmit={handleSubmit} style={{bottom:0, width:'100%', textAlign: 'center'}}>
+        <input
+          type="text"
+          placeholder="Enter Task"
+          value={newTask}
+          onChange={e => setNewTask(e.target.value)}
+        />
+        <button style={{margin : 10, padding:10, borderRadius: 5, color: 'black'}} type="submit" onClick={countTask}>Add to todo list</button>
+      </form>
     </>
   );
 }
+
+export default App;
