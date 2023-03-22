23b799a6bbfd9035d6256845d2605d630156f033
therohanmehta <rohandhn786@gmail.com>
2023-03-22 12:40:03 +0530

    this is the assignment solution of react navbar assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { BsMenuButtonWide } from "react-icons/bs";
+import { ImCross } from "react-icons/im";
 
+import Menu  from "./menu";
 function App() {
+  const [menu ,setMenu]=useState(false)
+  function visibleMenu(){
+    setMenu(true)
+  }
+  function hideMenu(){
+    setMenu(false)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <navbar className="mainNavbar" style={{ display: "flex" }}>
+        <img className='webIcon'
+          
+          src="https://2.bp.blogspot.com/-EXdg7tSnwOk/W3aO1VMbOsI/AAAAAAAAA7c/TGzElpLzolIYWwmXhz4mCYxHLZQIKCnpACLcBGAs/s1600/mylogo.jpg"
+          alt=""
+        />
+        <ul className="navIcons">
+          <li>Home</li>
+          <li>Service</li>
+          <li>Contact</li>
+          <li>About Us</li>
+        </ul>
+
+       { menu==true? <Menu/> : ""}
+        
+        <button className="loginBtn">Login</button>
+        <button className="signBtn">Signup</button>
+       
+        {menu==false? <button onClick={visibleMenu} className="visibleMenu">
+      <GiHamburgerMenu/></button> : <button className="hideMenu" onClick={hideMenu}><ImCross/></button>
+        }
+      </navbar>
+
+      
     </div>
   );
 }
---
+++ src/menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <div>
+      <ul className="hamNavIcons">
+          <li>Home</li>
+          <li>Service</li>
+          <li>Contact</li>
+          <li>About Us</li>
+        </ul>
+    </div>
+  )
+}
