7c7cfb5d5b9f6cb0c0ddbb46a32237478ff490ec
therohanmehta <rohandhn786@gmail.com>
2023-03-29 06:59:41 +0530

    This is the solution of Assignment which is given on 28/03

---
+++ src/Data/QuizData.js
@@ -0,0 +1,53 @@
+export const QuizData = [
+    {
+        question: "What is React?",
+        options:[ "Application", "Library", "Package", "JavaScript"],
+        answer: 2
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: "what is capital of India",
+        options: ["mumbai", "pune", "delhi", "none of the above"],
+        answer:3
+    },
+    {
+        question: "Demo Question ans Option2",
+        options: ["option1", "option 2", "option3", "none of the above"],
+        answer:2
+    },
+    {
+        question: "Demo Question ans Option2",
+        options: ["option1", "option 2", "option3", "none of the above"],
+        answer:2
+    },
+    {
+        question: "Demo Question ans Option2",
+        options: ["option1", "option 2", "option3", "none of the above"],
+        answer:2
+    },
+    {
+        question: "Demo Question ans Option2",
+        options: ["option1", "option 2", "option3", "none of the above"],
+        answer:2
+    },
+    {
+        question: "Demo Question ans Option2",
+        options: ["option1", "option 2", "option3", "none of the above"],
+        answer:2
+    },
+    
+];
